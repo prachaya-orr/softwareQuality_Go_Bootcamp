efdbbb90af55dc280e9f7f2951635ee4c7fb609a
prachayaOrr <prachaya.orr@gmail.com>
2022-12-04 11:55:53 +0700

    add dataType to prevent invalid age

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -5,19 +5,8 @@ import "testing"
 func TestTicketPrice(t *testing.T) {
 
 	t.Run("shoud return 0 when age is 0", func(t *testing.T) {
-		want := 13.0
-		age := -1
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(0) = %f; want %f", got, want)
-		}
-	})
-
-	t.Run("shoud return 0 when age is 0", func(t *testing.T) {
 		want := 0.0
-		age := 0
+		var age uint = 0
 
 		got := Price(age)
 
@@ -28,9 +17,8 @@ func TestTicketPrice(t *testing.T) {
 
 	t.Run("Free Ticket when age under 3", func(t *testing.T) {
 		want := 0.0
-		age := 3
 
-		got := Price(age)
+		got := Price(3)
 
 		if got != want {
 			t.Errorf("Price(3) = %f; want %f", got, want)
@@ -39,9 +27,8 @@ func TestTicketPrice(t *testing.T) {
 
 	t.Run("Ticket $15 when age is 4 year old", func(t *testing.T) {
 		want := 15.0
-		age := 4
 
-		got := Price(age)
+		got := Price(4)
 
 		if got != want {
 			t.Errorf("Price(4) = %f; want %f", got, want)
@@ -60,9 +47,8 @@ func TestTicketPrice(t *testing.T) {
 
 	t.Run("Ticket $30 when age over 15 ", func(t *testing.T) {
 		want := 30.0
-		age := 16
 
-		got := Price(age)
+		got := Price(16)
 
 		if got != want {
 			t.Errorf("Price(16) = %f; want %f", got, want)
